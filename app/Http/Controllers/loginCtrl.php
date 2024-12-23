eb32f3da622c9473f73a1d63298bc7eb65857df3
Nagaseh01 <jaysonsilverio2@gmail.com>
2024-12-23 15:11:15 +0800

    login-function-added

---
+++ app/Http/Controllers/loginCtrl.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class loginCtrl extends Controller
+{
+    //
+    public function act_login(Request $req)
+    {
+        dd($req);
+    }
+}
